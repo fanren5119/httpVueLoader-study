02b504ea2b38918f9a5450486336d0f067a7d7b2
fanren5119 <m15529399250@163.com>
2022-01-25 11:00:48 +0800

    上传项目

---
+++ http-vue-loader/html/my-component.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="hello" @click="respondsToTap">Hello {{count}}</div>
+</template>
+
+<script>
+	module.exports = {
+		data: function() {
+			return {
+				count: 0
+			}
+		},
+		methods: {
+			respondsToTap() {
+				this.count = this.count + 1
+			}
+		}
+	}
+</script>
+
+<style>
+	.hello {
+		background-color: #ffe;
+		line-height: 40px;
+	}
+</style>
---
+++ http-vue-loader/js/test.js
@@ -0,0 +1,17 @@
+(function() {
+	// 这句必须，引入httpVueLoader的；
+	Vue.use(httpVueLoader);
+	var vue = new Vue({
+		el: '#app',
+		data() {
+			return {
+
+			}
+		},
+		// components里面，引入组件
+		// 组件必须与html文件在同一目录下，不然下边的写法，会引入失败；
+		components: {
+			'my-component': 'url:my-component.vue?v=' + (new Date().getTime())
+		},
+	});
+})();
